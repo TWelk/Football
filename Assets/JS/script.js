1400a367ac4bcba4f18c7826d9aa6b8b1680be0e
Tyler Welker <twelker08@gmail.com>
2023-06-26 10:11:59 -0400

    testing website

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,14 +1,14 @@
-const seasonScheduleAPI = "http://api.sportradar.us/nfl/official/trial/v7/en/games/2022/REG/schedule.json?api_key=xywbpqjaddddgejnq8gjqmsn"
+const seasonScheduleAPI = "https://api.sportradar.us/nfl/official/trial/v7/en/games/2022/REG/schedule.json?api_key=xywbpqjaddddgejnq8gjqmsn"
 const weeklySchedule = "https://api.sportradar.us/nfl/official/trial/v5/en/games/2022/reg/02/schedule.JSON?api_key=xywbpqjaddddgejnq8gjqmsn"
-// const seasonSchedule = {
-//     scheduled: '',
-//     team1: '',
-//     team2: '',
-//     team1Score: '',
-//     team2Score: '',
-//     status: '',
-//     sequence: ''
-// }
+    // const seasonSchedule = {
+    //     scheduled: '',
+    //     team1: '',
+    //     team2: '',
+    //     team1Score: '',
+    //     team2Score: '',
+    //     status: '',
+    //     sequence: ''
+    // }
 
 const searchWeek = document.querySelector('#searchbtn')
 const selectedWeek = document.querySelector("#weekSelected")
@@ -16,7 +16,7 @@ const when1El = document.querySelector("#when1")
 let w
 
 var game = {
-    0:{
+    0: {
         dateAndTime: '',
         team1: '',
         team2: '',
@@ -169,7 +169,7 @@ searchWeek.addEventListener('click', function(e) {
     w = w.split('Week ')[1]
     var tbody = document.getElementsByClassName('active')
     tbody[0].classList.remove('active')
-    var weekEl = document.getElementById('week'+ w)
+    var weekEl = document.getElementById('week' + w)
     weekEl.classList.add('active')
     getSeasonSchedule(w)
 })
@@ -184,12 +184,12 @@ function getSeasonSchedule(w) {
         .then((data) => {
             w = w - 1
             game[w].dateAndTime.value = data.weeks[w].games[w].scheduled
-            // games[0].team1.value = data.
-            // games[0].team2.value = data.
-            // games[0].team1Score.value = data.
-            // games[0].team2Score.value = data.
-            // games[0].status.value = data.
-            // games[0].sequence.value = data.
+                // games[0].team1.value = data.
+                // games[0].team2.value = data.
+                // games[0].team1Score.value = data.
+                // games[0].team2Score.value = data.
+                // games[0].status.value = data.
+                // games[0].sequence.value = data.
         })
         .then(function() {
             displayGames()
